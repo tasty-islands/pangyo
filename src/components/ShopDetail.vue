0de975c18516973b2bb2d577af2f61e42564dff8
moonspam <booboo0412@gmail.com>
2023-05-16 23:02:08 +0900

    샵디테일 ui 레이아웃 추가

--- src/components/ShopDetail.vue
+++ src/components/ShopDetail.vue
@@ -1,14 +1,46 @@
 <script></script>
 
 <template>
-  <div>
-    <h2>김밥나라</h2>
-    <p>김밥</p>
+  <div class="text-center">
+    <h2 class="important-text-10">판교순대</h2>
+    <p>
+      <small
+        class="inline-block vertical-top px-2 py-1 bg-yellow-200 color-amber-900 border-rounded"
+        >🗺 NS 지하</small
+      >
+    </p>
+    <div class="mt-3 p-3 border-1 shadow-md">
+      <h3 class="important-text-3 font-bold">추천 음식</h3>
+      <hr class="my-3" />
+      <ul>
+        <li>맑은 순대국</li>
+        <li>맑은 순대국</li>
+      </ul>
+    </div>
+    <div class="mt-3 p-3 border-1 shadow-md">
+      <h3 class="important-text-3 font-bold">가격대</h3>
+      <hr class="my-3" />
+      <ul>
+        <li>특황등순대국 9,680</li>
+        <li>2인 : 25,520 (+1520원 오버)</li>
+        <li>3인 : 37,840 (+1840원 오버)</li>
+      </ul>
+    </div>
+    <div class="mt-3 p-3 border-1 shadow-md">
+      <h3 class="important-text-3 font-bold">코멘트</h3>
+      <hr class="my-3" />
+      <ul class="px-5 text-left list-disc">
+        <li>맑은 순대국으로 유명함</li>
+        <li>추가금 없이 먹으려면 특황등순대국 + 탄산을 드시면 11,440원</li>
+        <li>
+          순대국은 기본적으로 선지, 순대, 고기 3종이 섞여서 나오지만 '고기만
+          주세요'와 같이 커스텀 주문도 가능함
+        </li>
+        <li>메뉴가 늦게 나옴</li>
+      </ul>
+    </div>
+    <div class="mt-3 text-8"><a href="#">👍</a> 200</div>
   </div>
 </template>
 
-<style scoped>
-h2 {
-  font-size: 20px;
-}
-</style>
+<style scoped></style>
