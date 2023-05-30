3fa59ddaf85d996458f27f23449fbab82a396f02
Soyoung Kim <cheza1220@gmail.com>
2023-05-30 17:29:27 +0900

    feat: report query 로직 추가

--- src/components/Report.svelte
+++ src/components/Report.svelte
@@ -1,29 +1,44 @@
 <script>
-import Modal from './Modal.svelte';
- let showModal = false;
-  
-  const toggleModal = () => {
-    showModal = !showModal;
-  };
 
-  function handleWindowKeyDown(event) {
-		if (event.key === 'Escape' && showModal) {
-      console.log(event)
-			showModal = false
-		}
-	}
+import ReportList from './ReportList.svelte';
+import { setClient } from "svelte-apollo";
+import { createApolloClient } from "../graphql/apollo";
+
+const {
+  PUBLIC_TOKEN
+} = import.meta.env
+
+export let authToken = PUBLIC_TOKEN;
+const client = createApolloClient(authToken);
+setClient(client);
+
+// let showModal = false;
+  
+// const toggleModal = () => {
+//   showModal = !showModal;
+// };
+
+// function handleWindowKeyDown(event) {
+// 	if (event.key === 'Escape' && showModal) {
+//     console.log(event)
+// 		showModal = false
+// 	}
+// }
+
+
 </script>
 
-<p>
-  <Modal {showModal} on:click={toggleModal}>
+<div>
+  <!-- <Modal {showModal} on:click={toggleModal}>
     <h3>오류정보 신고하기</h3>
     <form class="flex flex-col">
       <textarea placeholder="내용을 입력해주세요. (ex.메뉴정보가 틀립니다.)" rows="4" class="mt-3 p-3 border-1 shadow-md" />
       
       <button class="mt-3 inline-block vertical-top px-2 py-1 bg-sky-500 border-rounded">신고하기</button>
     </form>
-  </Modal>
-  <button on:click={toggleModal}>오류정보 신고하기</button>
+  </Modal> -->
+  <button>오류정보 신고하기</button>
+  <ReportList />
   
-</p>
-<svelte:window on:keydown={handleWindowKeyDown}/>
+
+</div>
---
+++ src/components/ReportList.svelte
@@ -0,0 +1,29 @@
+<script>
+import { GET_REPORT } from "../graphql/queries";
+import { query } from "svelte-apollo";
+
+const id = 1;
+const reportsQuery = query(GET_REPORT, {
+  variables: {
+    id: 1
+  }
+});
+</script>
+
+
+<div class="todoListWrapper">
+<ul>
+  {#if $reportsQuery.loading}
+    <li>Loading...</li>
+  {:else if $reportsQuery.error}
+    <li>ERROR: {$reportsQuery.error.message}</li>
+  {:else}
+    {#each $reportsQuery.data.report as item (item.id)}
+      <li>{item.comment} by {item.type}</li>
+    {/each}
+  {/if}
+</ul>
+
+</div>
+
+
