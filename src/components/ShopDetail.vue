3a0e79883be285a3042cb401fcecde947cac6a2b
totol901 <savtotol901@gmail.com>
2023-06-09 18:58:46 +0900

    상세화면 수정
    -header 뒤로가기, 기타 기능 추가
    -식당 정보 디자인
    -필요 작업: github 댓글 디자인

--- src/components/ShopDetail.vue
+++ src/components/ShopDetail.vue
@@ -2,17 +2,64 @@
 
 <template>
   <div class="text-center">
-    <h2 class="important-text-10">판교순대</h2>
-    <p>
-      <small
-        class="inline-block vertical-top px-2 py-1 bg-yellow-200 color-amber-900 border-rounded mr-2"
-        >🗺 NS 지하</small
-      >
-      <small
-        class="inline-block vertical-top px-2 py-1 bg-green-200 color-amber-900 border-rounded"
-        >10% 할인</small
-      >
-    </p>
+    <div class="text-left">
+      <button><i class="arrow left" style="float: left"></i></button>
+
+      <div id="head-menu" style="float: right">
+        <ul id="head-menu">
+          <li>
+            <a href="#" class="important-text-6">⋯</a>
+            <ul>
+              <li>
+                <button class="mt-3 p-3 border-1 shadow-md">
+                  오류정보 신고하기
+                </button>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+      <hr class="my-3" />
+    </div>
+
+    <br />
+
+    <div class="text-left">
+      <h3 class="important-text-5 font-bold">순우가</h3>
+      <p>
+        <small
+          class="inline-block vertical-top py-1 color-amber-400 font-bold mr-2"
+          >한식</small
+        >
+        <small
+          class="inline-block vertical-top py-1 color-amber-500 font-bold mr-2"
+          >페이코 식원 가능</small
+        >
+      </p>
+      <p>
+        <small
+          class="inline-block vertical-top py-1 font-bold mr-2"
+          style="color: #757575"
+          >경기 성남시 분당구 대왕판교로 670 1층 119호</small
+        >
+      </p>
+      <p>
+        <small
+          class="inline-block vertical-top py-1 font-bold mr-2"
+          style="color: #757575"
+          >031-739-8300</small
+        >
+      </p>
+      <p>
+        <small
+          class="inline-block vertical-top py-1 font-bold mr-2 p-1"
+          style="color: #757575"
+          >💬45</small
+        >
+      </p>
+    </div>
+
     <div class="mt-3 p-3 border-1 shadow-md">
       <h3 class="important-text-3 font-bold">추천 음식</h3>
       <hr class="my-3" />
@@ -47,4 +94,43 @@
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.arrow {
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 10px;
+}
+
+.left {
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
+}
+
+ul li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+#head-menu {
+  margin: 0 auto;
+  text-align: center;
+}
+#head-menu > ul > li {
+  float: left;
+  position: relative;
+}
+#head-menu > ul > li > ul {
+  display: none;
+  width: 180px;
+  position: absolute;
+  top: 25px;
+  right: 5px;
+  background: white;
+}
+#head-menu > ul > li:hover > ul {
+  display: block;
+}
+</style>
