abacf9191ca6d68a29ef7eeb900e8159763cbe01
dlckdduq1107 <dlckdduq97@gmail.com>
2023-06-13 23:47:43 +0900

    feat: 식당 추천 컴포넌트 일부완성
    grphql요청 데이터 기반 컴포넌트 렌더링
    스타일 수정 및 적용

--- src/components/RecommandList.tsx
+++ src/components/RecommandList.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { EachRecommand } from './eachRecommand'
+import '../assets/styles/recommandList.css'
+
 declare global {
   namespace JSX {
     interface IntrinsicElements {
@@ -10,14 +12,59 @@ declare global {
     }
   }
 }
+const query = `
+   {
+    restaurant {
+      discount
+      payco
+      id
+      address
+      category
+      location
+      name
+      url
+    }
+  }
+`
 
-export default function RecommandList() {
+export default function RecommandList({ title }) {
+  const [restaurantList, setRestaurantList] = useState([])
+  const endPoint = 'https://tasty.hasura.app/v1/graphql'
+
   useEffect(() => {
     customElements.define('each-recommand', EachRecommand)
+
+    fetch(endPoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: '',
+      },
+      body: JSON.stringify({ query }),
+    })
+      .then(res => res.json())
+      .then(json => {
+        setRestaurantList(json.data.restaurant)
+      })
   }, [])
+
   return (
     <div>
-      <each-recommand></each-recommand>
+      <div>{title}</div>
+      <div className="recommand-list-wrapper">
+        {restaurantList.map(
+          ({ discount, payco, id, address, category, location, name, url }) => {
+            return (
+              <each-recommand
+                image-url="https://github.com/tasty-islands/pangyo/assets/67806982/fe94c433-6e89-4c80-827a-1bb387652ef3"
+                title={name}
+                category-name={category}
+                key={id}
+              ></each-recommand>
+            )
+          },
+        )}
+      </div>
     </div>
   )
 }
--- src/components/eachRecommand.ts
+++ src/components/eachRecommand.ts
@@ -6,7 +6,7 @@ export class EachRecommand extends HTMLElement {
   connectedCallback() {
     const shadow = this.attachShadow({ mode: 'open' })
 
-    shadow.innerHTML = `<div>Hello world!</div>`
+    shadow.innerHTML = this.makeTemplate()
 
     this.insertStyleInnerShadowDom(shadow)
   }
@@ -19,18 +19,51 @@ export class EachRecommand extends HTMLElement {
     console.log('Custom square element moved to new page.')
   }
 
+  static get observedAttributes() {
+    return []
+  }
+
   attributeChangedCallback(name, oldValue, newValue) {
     console.log('Custom square element attributes changed.')
   }
 
-  insertStyleInnerShadowDom(shadowDom) {
+  insertStyleInnerShadowDom(shadowDom: ShadowRoot) {
     const style = document.createElement('style')
 
     style.textContent = `
-    div{
-      color: red;
-    }`
+    .each-res-wrapper{
+      margin: 5px;
+    }
+    .info-wrapper{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: flex-end;
+    }
+    .info-title{
+      font-size: 12px;
+      font-weight: bold;
+    }
+    .info-category{
+      color:#FB5B00;
+      font-size: 10px;
+    }
+    `
 
     shadowDom.appendChild(style)
+  }
+
+  makeTemplate() {
+    return `<div class='each-res-wrapper'>
+      <img src=${this.getAttribute('image-url')} width=138 height=84/>
+      <div class='info-wrapper'>
+        <div class='info-title'>
+          ${this.getAttribute('title')}
+        </div>
+        <div class='info-category'>
+          ${this.getAttribute('category-name')}
+        </div>
+      </div>
+    </div>`
   }
 }
