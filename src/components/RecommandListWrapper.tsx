7ae5a5f20e9ca1a5951399d044604680c8e29fca
dlckdduq1107 <dlckdduq97@gmail.com>
2023-06-21 11:53:07 +0900

    feat: 위치 연결 및 주변 식당 추천(5개)

--- src/components/RecommandListWrapper.tsx
+++ src/components/RecommandListWrapper.tsx
@@ -2,17 +2,34 @@
 import React, { useEffect, useState } from 'react'
 import { EachRecommand } from './eachRecommand'
 import RecommandList from './RecommandList'
+import { NK, watchLocation } from '@/utils/geo'
 
 const RecommandListWrapper = () => {
   const [restaurantList, setRestaurantList] = useState([])
   const [listFromGeo, setListFromGeo] = useState([])
   const [randomList, setRandomList] = useState([])
   const endPoint = 'https://tasty.hasura.app/v1/graphql'
+  const [currentLocation, setCurrentLocation] = useState([
+    NK.latitude,
+    NK.longitude,
+  ])
+
+  useEffect(() => {
+    watchLocation(coord => {
+      const latitude = coord.latitude
+      const longitude = coord.longitude
+      setCurrentLocation([latitude, longitude])
+    })
+  }, [])
 
   useEffect(() => {
     customElements.define('each-recommand', EachRecommand)
     getGraphqlData()
   }, [])
+
+  useEffect(() => {
+    setListFromGeo(getListFromGeo(restaurantList))
+  }, [restaurantList.length])
 
   const getGraphqlData = async () => {
     const res = await fetch(endPoint, {
@@ -28,15 +45,35 @@ const RecommandListWrapper = () => {
     setRestaurantList(data.data.restaurant)
   }
 
-  const getListFromGeo = () => {}
+  const calculateDistance = (x1, y1, x2, y2) => {
+    const distance = Math.sqrt(Math.abs(x1 - x2) ** 2 + Math.abs(y1 - y2) ** 2)
+    return distance
+  }
+  const getListFromGeo = originList => {
+    const distanceList = originList.map(val => {
+      const [restaurantX, restaurantY] = val.location.split(',')
+      return {
+        ...val,
+        distance: calculateDistance(
+          restaurantX,
+          restaurantY,
+          currentLocation[0],
+          currentLocation[1],
+        ),
+      }
+    })
+    distanceList.sort((a, b) => {
+      return a.distance > b.distance ? 1 : -1
+    })
+
+    return distanceList.slice(0, 5)
+  }
   const getListRandom = () => {}
 
   return (
     <div className="total-recommand-wrapper">
-      <RecommandList
-        title="내 주변 식당 추천"
-        restaurantList={restaurantList}
-      />
+      <div>{`${currentLocation[0]},${currentLocation[1]}`}</div>
+      <RecommandList title="내 주변 식당 추천" restaurantList={listFromGeo} />
       <RecommandList
         title="오늘의 식당 랜덤 추천"
         restaurantList={restaurantList}
